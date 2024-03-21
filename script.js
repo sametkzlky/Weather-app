f1038738da7d3a45d850bf3b317b3958542234b8
sametkzlky <119797243+sametkzlky@users.noreply.github.com>
2024-03-21 21:54:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,35 +9,41 @@ const weathericon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   const response = await fetch(apiurl + city + `&appid=${apikey}`);
 
-if(response.status==404){
-    document.querySelector(".error").style.display="block";
-    document.querySelector(".weather").style.display="none";
-   
-}else{
-
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
     var data = await response.json();
-    
-  console.log(data);
+
+    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-    
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+
     if (data.weather[0].main == "Clouds") {
       weathericon.src = "images/cloud.png";
     } else if (data.weather[0].main == "Clear") {
       weathericon.src = "images/clear.png";
     } else if (data.weather[0].main == "Rain") {
       weathericon.src = "images/rain.png";
-    }else if (data.weather[0].main == "Snow") {
+    } else if (data.weather[0].main == "Snow") {
       weathericon.src = "images/snow.png";
     }
-    
-    document.querySelector(".weather").style.display="block";
-    document.querySelector(".error").style.display="none";
 
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
-}
+// Arama butonunun click olayı
 searchbtn.addEventListener("click", () => {
   checkWeather(searchbox.value);
+});
+
+// Enter tuşuna basıldığında arama butonunun tetiklenmesi
+searchbox.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    searchbtn.click();
+  }
 });
